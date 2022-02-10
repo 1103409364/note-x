a95c310078d54ad1d0adf61e2051da4b079f0b9d
wcs <1103409364@qq.com>
2022-02-10 13:24:28 +0800

    部署配置
    Signed-off-by: wcs <1103409364@qq.com>

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -18,5 +18,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:1103409364/note-x.git master:gh-pages
 
 cd -
